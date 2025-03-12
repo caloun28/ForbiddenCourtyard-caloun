86f55cd68d98e8d33418d630430aa14dee742d4b
Xelor-911 <caloun@spsejecna.cz>
2025-03-12 21:32:07 +0100

    inventory limited to only 3 spaces

--- src/player/Inventory.java
+++ src/player/Inventory.java
@@ -5,13 +5,27 @@ import entity.items.Item;
 import java.util.HashMap;
 
 public class Inventory {
-    private HashMap<String, Item> inventory;
+    private HashMap<String, Item> inventory = new HashMap<>();
+    private int maxItems = 3;
 
-    public void addItem(Item item) {
+    public boolean addItem(Item item) {
+        if (inventory.size() >= maxItems) {
+            return false;
+        }
         inventory.put(item.getName(), item);
+        return true;
     }
 
-    public void removeItem(Item item) {
-        inventory.remove(item.getName());
+    public HashMap<String, Item> getItems() {
+        return inventory;
+    }
+
+    public void removeItem(String itemName) {
+        inventory.remove(itemName);
+    }
+
+    @Override
+    public String toString() {
+        return "Predmety : " + inventory;
     }
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -4,27 +4,28 @@ import entity.Npc;
 import entity.items.Axe;
 import entity.items.Bottle;
 import entity.items.Medkit;
+import world.Location;
+
+import java.util.Random;
 
 public class Player {
-    private String name;
     private int health;
     private int damage;
     private boolean alive;
+    private Location currentLocation;
+    private Inventory inventory;
+    private Random rand = new Random();
+    private int maxHealth;
 
-    public Player(String name, int health, int damage, boolean alive) {
-        this.name = name;
+    public Player(int health, int damage, boolean alive) {
         this.health = health;
         this.damage = damage;
         this.alive = alive;
+        this.currentLocation = null;
+        this.inventory = new Inventory();
+        this.maxHealth = health;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getHealth() {
-        return health;
-    }
 
     public int getDamage() {
         return damage;
@@ -34,15 +35,58 @@ public class Player {
         return alive;
     }
 
-    public void setHealth(int health) {
-        this.health = health;
+    public int getHealth() {
+        return health;
     }
 
-    public void increaseHealth(Medkit medkit) {
-        if(!medkit.isUsed()) {
-            this.health += medkit.getPlusHealth();
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public void setHealth(int health) {
+        if(health > 0) {
+            this.health = health;
+        }else{
+            this.alive = false;
         }
-        medkit.setUsed(true);
+    }
+
+    public void healToMax() {
+        this.health = maxHealth;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+
+    public void moveTo(Location newLocation) {
+        this.currentLocation = newLocation;
+    }
+
+    public void setCurrentLocation(Location location) {
+        this.currentLocation = location;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+
+    public void increaseHealth(Medkit medkit) {
+        if (!medkit.isUsed()) {
+            this.health += medkit.getPlusHealth();
+
+            if (this.health > maxHealth) {
+                this.maxHealth = this.health;
+            }
+
+            medkit.setUsed(true);
+        }
     }
 
     public void increaseDamage(Axe axe){
@@ -50,12 +94,18 @@ public class Player {
     }
 
     public void attack(Npc npc) {
-        npc.setHealth(npc.getHealth() - damage);
+        int realDamage = damage+ rand.nextInt(7);
+        npc.setHealth(npc.getHealth() - realDamage);
     }
 
     public void increaseEndurance(Npc npc, Bottle bottle) {
         if(!bottle.isUsed()) {
             npc.setDamage(npc.getDamage() / bottle.getQuality());
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Mas " + getHealth() + "HP, tvoje utoky davaji za " + getDamage() + " a v inventari mas " + getInventory();
     }
 }
